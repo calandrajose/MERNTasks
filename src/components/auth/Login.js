08f4567df54a78e12a412f3d5b2f2e7ef92ae715
Jose Ignacio Calandra <calandrajosei@gmail.com>
2021-03-23 12:22:33 -0300

    login and signup structure complete

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Link} from 'react-router-dom'
 
 const Login = () => {
-    const onChangeHandler = (e)=>{
-        
-    }
-    return (
-        <div className='form-user' >
-            <div className='container-form shadow-dark'>
-                <h1>Iniciar Sesion</h1>
-                <form>
-                    <div className='field-form'>
-                        <label htmlFor='email'>Email</label>
-                        <input
-                            type='email'
-                            id='email'
-                            name='email'
-                            placeholder='Tu email'
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  //Login state
+  const [credentials, setCredentials] = useState({
+      email: '',
+      password: ''
+  })
+
+  const {email, password} = credentials
+
+  const onChangeHandler = e => {
+      setCredentials({
+          ...credentials,
+          [e.target.name]: e.target.value
+      })
+  };
+
+  const onSubmitHandler = e =>{
+      e.preventDefault()
+  }
+  return (
+    <div className="form-user">
+      <div className="container-form shadow-dark">
+        <h1>Iniciar Sesion</h1>
+        <form>
+          <div className="field-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className="field-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu password"
+              value={password}
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className="field-form">
+            <input 
+                type='btn' 
+                className='btn btn-primary btn-block'
+                value='Iniciar Sesión' 
+                onSubmit={onSubmitHandler}   
+            />
+          </div>
+        </form>
+        <Link to={'/nueva-cuenta'} className='link-account'>
+            Obtener Cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Link} from 'react-router-dom'
 
 const NewAccount = () => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirm: ''
+    })
+
+    const { username, email, password, confirm } = credentials
+
+    const onChangeHandler = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const onSubmitHandler = e => {
+        e.preventDefault()
+    }
     return (
-        <h1>Desde nueva cuenta</h1>
-    );
-};
+        <div className="form-user">
+            <div className="container-form shadow-dark">
+                <h1>Registrarse</h1>
+                <form>
+                <div className="field-form">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Username"
+                            value={username}
+                            onChange={onChangeHandler}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu email"
+                            value={email}
+                            onChange={onChangeHandler}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu password"
+                            value={password}
+                            onChange={onChangeHandler}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="confirm">Confirmar Password</label>
+                        <input
+                            type="password"
+                            id="confirm"
+                            name="confirm"
+                            placeholder="Repite tu password"
+                            value={confirm}
+                            onChange={onChangeHandler}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <input
+                            type='btn'
+                            className='btn btn-primary btn-block'
+                            value='Iniciar Sesión'
+                            onSubmit={onSubmitHandler}
+                        />
+                    </div>
+                </form>
+                <Link to={'/'} className='link-account'>
+                    Volver a Iniciar Sesión
+          </Link>
+            </div>
+        </div>
+    )
+}
 
 export default NewAccount;
