2e0024bc528f2b8f421aa0ea287e169977e8fb81
Jose Ignacio Calandra <calandrajosei@gmail.com>
2021-03-27 15:33:10 -0300

    add username bar

---
+++ src/components/layout/Bar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Bar = () => {
+    return (
+        <div>
+            <header className='app-header'>
+                <p className='user-name'>Hola <span>Jose Ignacio</span></p>
+                <nav className='main-nav'>
+                    <a href='#!'>Cerrar Sesion</a>
+                </nav>
+            </header>
+        </div>
+    );
+};
+
+export default Bar;
